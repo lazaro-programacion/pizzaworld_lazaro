7fe73704804c3dfb22fde7c5591b3d44dbcec614
lazaro <lazaro0089@gmail.com>
2020-02-28 10:02:34 +0100

    parriba

--- pizaworld/src/main/java/com/pizzaworld/pizaworld/controller/VistaPizzaControlador.java
+++ pizaworld/src/main/java/com/pizzaworld/pizaworld/controller/VistaPizzaControlador.java
@@ -1,13 +1,9 @@
 package com.pizzaworld.pizaworld.controller;
 
-import com.pizzaworld.pizaworld.Entity.Ingrediente;
 import com.pizzaworld.pizaworld.repository.IngredientesRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,28 +29,6 @@ public class VistaPizzaControlador {
         return modelAndView;
     }
 
-    @PostMapping("/ingredientes")
-    public ModelAndView showListaIngredientesAndViewPost(@RequestParam("nombre") String nombre,
-             @RequestParam("precio") Float precio) {
-
-        {
-            ModelAndView mdl = new ModelAndView();
-            try {
-
-                mdl.setViewName("Ingredientes");
-                Ingrediente ingrediente = new Ingrediente(nombre, precio);
-                ir.save(ingrediente);
-                mdl.addObject("ingrediente", ir.findAll());
-
-                mdl.addObject("mensaje", "");
-
-                return mdl;
-            } catch (Exception e) {
-                mdl.setViewName("404");
-                mdl.addObject("errormsg", e.getMessage());
-                return mdl;
-            }
-        }
-
-    }
+   
+ 
 }
